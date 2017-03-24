5aada8943e2439eee3dd3002b189a0422322908f
Zofia <zofjanicka@gmail.com>
2017-03-24 23:15:10 +0100

    layouty

--- app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
+++ app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.github.braincode17.giftapp;
 
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/com/github/braincode17/giftapp/SingleItem.java
@@ -0,0 +1,37 @@
+package com.github.braincode17.giftapp;
+
+import java.math.BigDecimal;
+
+public class SingleItem {
+
+    private String itemId;
+    private String itemImage;
+    private String itemTitle;
+    private String itemPrice;
+    private String shippingPrice;
+    private String shippingTime;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public String getItemTitle() {
+        return itemTitle;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public String getShippingPrice() {
+        return shippingPrice;
+    }
+
+    public String getShippingTime() {
+        return shippingTime;
+    }
+}
