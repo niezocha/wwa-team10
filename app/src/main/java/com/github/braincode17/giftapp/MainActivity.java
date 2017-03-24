8a284328c70f8d43ce8b6f68fa40110fab7b0c23
w-kamil <w-kamil@wp.pl>
2017-03-24 23:29:07 +0100

    retrofit request first

--- app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
+++ app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
@@ -1,31 +1,35 @@
 package com.github.braincode17.giftapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.stormpath.sdk.Stormpath;
-import com.stormpath.sdk.StormpathConfiguration;
-import com.stormpath.sdk.StormpathLogger;
+import com.github.braincode17.giftapp.SearchList.SearchService;
 
-import okhttp3.OkHttpClient;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String baseUrl = " https://ssl.allegro.pl/auth/oauth/token ";
+
+    String urlSearch = "http://inspiracje.allegro.pl/api/offers/";
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (BuildConfig.DEBUG) {
-            Stormpath.setLogLevel(StormpathLogger.VERBOSE);
-        }
-        StormpathConfiguration stormpathConfiguration = new StormpathConfiguration.Builder().baseUrl(baseUrl).build();
-        Stormpath.init(this, stormpathConfiguration);
+        retrofit = new Retrofit.Builder().addCallAdapterFactory(RxJava2CallAdapterFactory.create()).addConverterFactory(GsonConverterFactory.create()).baseUrl(urlSearch).build();
+
+        SearchService searchService = retrofit.create(SearchService.class);
+        searchService.search("dlaniej", "200", "random").subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe();
 
 
 
 
     }
+
+
 }
---
+++ app/src/main/java/com/github/braincode17/giftapp/SearchList/SearchResult.java
@@ -0,0 +1,14 @@
+package com.github.braincode17.giftapp.SearchList;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SearchResult {
+
+
+
+    private List<SingleSearchResult> items;
+
+}
