2652fb561a91ba3c24196d360d1a988495174b37
w-kamil <w-kamil@wp.pl>
2017-03-25 11:32:32 +0100

    poprawki

--- app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
+++ app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClick {
         SearchService searchService = retrofit.create(SearchService.class);
         NumberFormat format = NumberFormat.getInstance();
         format.setMinimumFractionDigits(2);
-                searchService.search(queryGenerator.getTag(), queryGenerator.getPrice(), queryGenerator.getSort())
+        searchService.search(queryGenerator.getTag(), queryGenerator.getPrice(), queryGenerator.getSort())
                 .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
                 .flatMap(Observable::fromIterable)
                 .map(singleSearchResult -> new BaseSearchResult(singleSearchResult.getItemId(), singleSearchResult.getGalleryImage().getUrl(), singleSearchResult.getName(),
