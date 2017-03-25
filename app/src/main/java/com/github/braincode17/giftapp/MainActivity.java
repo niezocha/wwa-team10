fde453f41fbe520c924295eae6f5bf08a0e9ae79
Zofia <zofjanicka@gmail.com>
2017-03-25 05:17:13 +0100

    t

--- app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
+++ app/src/main/java/com/github/braincode17/giftapp/MainActivity.java
@@ -26,7 +26,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,14 +38,23 @@ public class MainActivity extends AppCompatActivity {
 
     private String tag = "allegropl";
     private String price = "0";
-    private String listing = "random";
+    private String sort = "random";
     private String defUrl = "https://inspiracje.allegro.pl/api/";
     private String urlSearch = "http://inspiracje.allegro.pl/api/offers/";
     private Retrofit retrofit;
 
     private static final String[] tags = {"inspirującego","dla niej", "dla niego", "dla dziecka", "dla domu", "z prezentów"};
+    private static final String [] tagsVal = {"allegropl", "dlaniej", "dlaniego", "dziecko", "dom", "gadzet"};
+
     private static final String[] prices = {"dowolnej kwoty", "50 zł", "100 zł", "200 zł"};
+    private static final String[] pricesVal = {"0", "50", "100", "200"};
+
     private static final String[] sorting = {"losowo", "najtańszych", "najdroższych", "najpopularniejszych"};
+    private static final String[] sortingVal = {"random", "cheapest", "most_expencive", "most_popular"};
+
+    Map<String, String> tagsMap;
+    Map<String, String> pricesMap;
+    Map<String, String> sortingsMap;
 
     @BindView(R.id.view_pager)
     ViewPager viewPager;
@@ -60,6 +71,34 @@ public class MainActivity extends AppCompatActivity {
     private String tagSelected;
     private String priceSelected;
     private String sortSelected;
+
+
+    public void setTag() {
+        String tag = tagsMap.get(tagSelected);
+        this.tag = tag;
+    }
+
+    public void setPrice() {
+        String price = pricesMap.get(priceSelected);
+        this.price = price;
+    }
+
+    public void setSort() {
+        String sort = sortingsMap.get(sortSelected);
+        this.sort = sort;
+    }
+
+    public String getTagSelected() {
+        return tagSelected;
+    }
+
+    public String getPriceSelected() {
+        return priceSelected;
+    }
+
+    public String getSortSelected() {
+        return sortSelected;
+    }
 
     public void setTagSelected(String tagSelected) {
         this.tagSelected = tagSelected;
@@ -85,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(urlSearch).build();
 
-        updatedSearch(tag, price, listing);
+        updatedSearch(tag, price, sort);
 
         tabLayout.setupWithViewPager(viewPager);
         itemsList = new ArrayList<>();
@@ -94,6 +133,10 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ItemsPagerAdapter(itemsList, sharedPreferences);
         viewPager.setAdapter(adapter);
 
+
+        tagsMap = generateMap(tags, tagsVal);
+        pricesMap = generateMap(prices, pricesVal);
+        sortingsMap = generateMap(sorting, sortingVal);
 
     }
 
@@ -199,5 +242,12 @@ public class MainActivity extends AppCompatActivity {
         spinner.setVisibility(View.VISIBLE);
     }
 
+    public Map<String, String> generateMap(String[] list1, String[] list2){
+        Map<String, String> map = new HashMap<>();
+        for (int i=0; i<list1.length; i++){
+            map.put(list1[i], list2[i]);
+        }
+        return map;
+    }
 
 }
